725d4d395c5ef93092b87280768a6f30f7e605cd
samreysteven <samreysteven91@gmail.com>
2017-09-04 02:16:06 +0800

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -1,48 +1,29 @@
 #!/bin/bash
+#
+# SCRIPT BY ORANGKUATSABAHANTERKINI
+# WELCOME
+# ==================================================
 
+# download script
 cd /usr/bin
-
-wget -O menu "https://github.com/blazevpn/autoscript/blob/master/menu.sh"
-
-wget -O usernew "https://github.com/blazevpn/autoscript/blob/master/usernew.sh"
-
-wget -O trial "https://github.com/blazevpn/autoscript/blob/master/trial.sh"
-
-wget -O hapus "https://github.com/blazevpn/autoscript/blob/master/hapus.sh"
-
-wget -O login "https://github.com/blazevpn/autoscript/blob/master/user-login.sh"
-
-wget -O member "https://github.com/blazevpn/autoscript/commit/893dda0e47a14d77d550aa4cd2d7aa1b465d824d"
-
-wget -O resvis "https://github.com/blazevpn/autoscript/blob/master/resvis.sh"
-
-wget -O speedtest "https://github.com/blazevpn/autoscript/blob/master/speedtest_cli.py"
-
-wget -O about "https://github.com/blazevpn/autoscript/blob/master/about.sh"
-
+wget -O menu "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/menu.sh"
+wget -O usernew "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/usernew.sh"
+wget -O trial "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/trial.sh"
+wget -O hapus "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/hapus.sh"
+wget -O login "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/user-login.sh"
+wget -O member "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/user-list.sh"
+wget -O resvis "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/resvis.sh"
+wget -O speedtest "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/speedtest_cli.py"
+wget -O about "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/about.sh"
 echo "0 0 * * * root /usr/bin/reboot" > /etc/cron.d/reboot
-
 echo "* * * * * service dropbear restart" > /etc/cron.d/dropbear
-
 chmod +x menu
-
 chmod +x usernew
-
 chmod +x trial
-
 chmod +x hapus
-
 chmod +x login
-
 chmod +x member
-
 chmod +x resvis
-
 chmod +x speedtest
-
 chmod +x about
-screen -AmdS limit /root/limit.sh
-screen -AmdS ban /root/ban.sh
 clear
-cd
-echo "MENU SCRIPT BERJAYA DI INSTALL!"
